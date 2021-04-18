57dc03874f42bee68a6fb588b5f908eb7af78a51
Stefan Solberg <ssol@itu.dk>
2021-04-18 14:46:29 +0200

    2.3 done

--- train_pytorch.py
+++ train_pytorch.py
@@ -1,20 +1,37 @@
 from trainers import PyTorchTrainer
 import torch
 from torchvision import transforms
-from networks import Linear, MLP, BasicNetwork, MLP_many_layer
+from networks import Linear, MLP, BasicNetwork, MLP_many_layer, CNN, CNN4Layer, TopCNN
 
 
+CNN_model1 = CNN()
+CNN_Trainer1 = PyTorchTrainer(CNN_model1, transforms.ToTensor(), torch.optim.SGD(CNN_model1.parameters(), lr=0.01), 128)
+CNN_Trainer1.train(10)
+CNN_Trainer1.save()
+
+CNN_model2 = TopCNN()
+CNN_Trainer2 = PyTorchTrainer(CNN_model2, transforms.ToTensor(), torch.optim.SGD(CNN_model2.parameters(), lr=0.01), 128)
+CNN_Trainer2.train(10)
+CNN_Trainer2.save()
+
+CNN_model3 = CNN4Layer()
+CNN_Trainer3 = PyTorchTrainer(CNN_model3, transforms.ToTensor(), torch.optim.SGD(CNN_model3.parameters(), lr=0.01), 128)
+CNN_Trainer3.train(10)
+CNN_Trainer3.save()
+
+"""
 MLP_model1 = MLP()
-Trainer1 = PyTorchTrainer(MLP_model1, transforms.ToTensor(), torch.optim.SGD(MLP_model1.parameters(), lr=0.01), 128)
-Trainer1.train(10)
-Trainer1.save()
+MLP_Trainer1 = PyTorchTrainer(MLP_model1, transforms.ToTensor(), torch.optim.SGD(MLP_model1.parameters(), lr=0.01), 128)
+MLP_Trainer1.train(10)
+MLP_Trainer1.save()
 
 MLP_model2 = BasicNetwork()
-Trainer2 = PyTorchTrainer(model2, transforms.ToTensor(), torch.optim.SGD(MLP_model2.parameters(), lr=0.01), 128)
-Trainer2.train(10)
-Trainer2.save()
+MLP_Trainer2 = PyTorchTrainer(model2, transforms.ToTensor(), torch.optim.SGD(MLP_model2.parameters(), lr=0.01), 128)
+MLP_Trainer2.train(10)
+MLP_Trainer2.save()
 
 MLP_model3 = MLP_many_layer()
-Trainer3 = PyTorchTrainer(MLP_model3, transforms.ToTensor(), torch.optim.SGD(MLP_model3.parameters(), lr=0.01), 128)
-Trainer3.train(10)
-Trainer3.save()
+MLP_Trainer3 = PyTorchTrainer(MLP_model3, transforms.ToTensor(), torch.optim.SGD(MLP_model3.parameters(), lr=0.01), 128)
+MLP_Trainer3.train(10)
+MLP_Trainer3.save()
+"""
